7449ad15b88a4117858fe2ee56dd1ba5be68cc52
Nick Ball <nick@wolfninja.com>
2014-12-19 23:48:32 -0800

    Round out first set of contract tests

--- src/test/java/com/wolfninja/keystore/api/BaseKeyspaceTest.java
+++ src/test/java/com/wolfninja/keystore/api/BaseKeyspaceTest.java
@@ -1,5 +1,7 @@
 package com.wolfninja.keystore.api;
 
+import java.util.UUID;
+
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -17,7 +19,7 @@ public abstract class BaseKeyspaceTest {
 
 	@Test
 	public void addingAgainReturnsFalse() {
-		String key = "second.key";
+		final String key = genRandKey();
 		final boolean actual = keyspace.add(key, "some value");
 		Assert.assertTrue(actual, "Should be able to add a new key");
 		final boolean actual2 = keyspace.add(key, "other value");
@@ -25,33 +27,85 @@ public abstract class BaseKeyspaceTest {
 	}
 
 	@Test
-	public void checkAndSet() {
-		throw new RuntimeException("Test not implemented");
+	public void checkAndSetShouldReplaceWithSameVersion() {
+		final String key = genRandKey();
+
+		// Set initial value
+		final boolean resultSetting = keyspace.set(key, "mine!");
+		Assert.assertTrue(resultSetting);
+
+		// Get existing value
+		final Optional<KeyValue> insertedValue = keyspace.get(key);
+		Assert.assertTrue(insertedValue.isPresent());
+
+		// Check and set should return true
+		final boolean actual = keyspace.checkAndSet(key, "abcd", insertedValue.get().getVersion());
+		Assert.assertTrue(actual);
+		final Optional<KeyValue> actualVal = keyspace.get(key);
+		// Actual value should be set
+		Assert.assertTrue(actualVal.isPresent());
+		Assert.assertEquals("abcd", actualVal.get().getValue());
+	}
+
+	@Test
+	public void checkAndSetShouldReturnFalseOnMismatch() {
+		final String key = genRandKey();
+
+		// Set initial value
+		final boolean resultSetting = keyspace.set(key, "mine!");
+		Assert.assertTrue(resultSetting);
+
+		// Get existing value
+		final Optional<KeyValue> insertedValue = keyspace.get(key);
+		Assert.assertTrue(insertedValue.isPresent());
+
+		// Check and set should return false
+		final boolean actual = keyspace.checkAndSet(key, "abcd", insertedValue.get().getVersion() - 1L);
+		Assert.assertFalse(actual);
+		final Optional<KeyValue> actualVal = keyspace.get(key);
+		// Actual value should be unchanged
+		Assert.assertTrue(actualVal.isPresent());
+		Assert.assertEquals("mine!", actualVal.get().getValue());
+	}
+
+	@Test
+	public void checkAndSetShouldReturnFalseOnNewKey() {
+		final String key = genRandKey();
+
+		Assert.assertFalse(keyspace.exists(key));
+		// Check and set with new key, should do nothing
+		final boolean resultSetting = keyspace.checkAndSet(key, "mine!", 43L);
+		Assert.assertFalse(resultSetting);
+		Assert.assertFalse(keyspace.exists(key));
 	}
 
 	@Test
 	public void deletingNewKeyShouldReturnFalse() {
-		final String key = "fourth.key";
+		final String key = genRandKey();
 		Assert.assertFalse(keyspace.delete(key), "expecting false");
 	}
 
 	@Test
 	public void existsShouldReturnFalseIfNotExists() {
-		final String key = "sixth.key";
+		final String key = genRandKey();
 		Assert.assertFalse(keyspace.exists(key));
 	}
 
 	@Test
 	public void existsShouldReturnTrueIfExists() {
-		final String key = "fifth.key";
+		final String key = genRandKey();
 		Assert.assertFalse(keyspace.exists(key));
 		Assert.assertTrue(keyspace.add(key, "something"));
 		Assert.assertTrue(keyspace.exists(key));
 	}
 
+	private String genRandKey() {
+		return UUID.randomUUID().toString();
+	}
+
 	@Test
 	public void getShouldReturnAbsentIfDeleted() {
-		String key = "tenth.key";
+		final String key = genRandKey();
 		keyspace.add(key, "something awesome");
 		Assert.assertTrue(keyspace.exists(key));
 		keyspace.delete(key);
@@ -62,13 +116,13 @@ public abstract class BaseKeyspaceTest {
 
 	@Test
 	public void getShouldReturnAbsentIfNotPresent() {
-		final Optional<KeyValue> actual = keyspace.get("seventh.key");
+		final Optional<KeyValue> actual = keyspace.get(genRandKey());
 		Assert.assertFalse(actual.isPresent());
 	}
 
 	@Test
 	public void getShouldReturnDifferentVersionsIfChanged() {
-		String key = "ninth.key";
+		final String key = genRandKey();
 		keyspace.add(key, "something awesome");
 
 		final Optional<KeyValue> first = keyspace.get(key);
@@ -83,7 +137,7 @@ public abstract class BaseKeyspaceTest {
 
 	@Test
 	public void getShouldReturnIfPresent() {
-		String key = "eigth.key";
+		final String key = genRandKey();
 		keyspace.add(key, "my awesome value");
 
 		final Optional<KeyValue> actual = keyspace.get(key);
@@ -94,13 +148,46 @@ public abstract class BaseKeyspaceTest {
 	}
 
 	@Test
-	public void set() {
-		throw new RuntimeException("Test not implemented");
+	public void replacingExistingKeyWithSameValueShouldReturnFalse() {
+		final String key = genRandKey();
+		final String value = "woot";
+		keyspace.add(key, value);
+		Assert.assertFalse(keyspace.replace(key, value));
+		Assert.assertEquals(keyspace.get(key).get().getValue(), value);
+	}
+
+	@Test
+	public void setShouldAllowExistingKeys() {
+		final String key = genRandKey();
+
+		// Set value for key
+		{
+			final String value = "woot";
+			final boolean actual = keyspace.set(key, value);
+			Assert.assertTrue(actual);
+			Assert.assertEquals(keyspace.get(key).get().getValue(), value);
+		}
+		// Add to key again
+		{
+			final String expected = "newer";
+			final boolean actual = keyspace.set(key, expected);
+			Assert.assertTrue(actual);
+			Assert.assertEquals(keyspace.get(key).get().getValue(), expected);
+		}
+	}
+
+	@Test
+	public void setShouldAllowNewKeys() {
+		final String key = genRandKey();
+		final String value = "woot";
+		final boolean actual = keyspace.set(key, value);
+		Assert.assertTrue(actual);
+		Assert.assertEquals(keyspace.get(key).get().getValue(), value);
 	}
 
 	@Test
 	public void shouldBeAbleToAdd() {
-		final String key = "first.key";
+		final String key = genRandKey();
 		final String value = "some value";
 		final boolean actual = keyspace.add(key, value);
 		Assert.assertTrue(actual);
@@ -109,7 +196,7 @@ public abstract class BaseKeyspaceTest {
 
 	@Test
 	public void shouldBeAbleToDelete() {
-		final String key = "fourteenth.key";
+		final String key = genRandKey();
 		final boolean actualAdded = keyspace.add(key, "something");
 		Assert.assertTrue(actualAdded);
 		Assert.assertTrue(keyspace.exists(key));
@@ -120,7 +207,7 @@ public abstract class BaseKeyspaceTest {
 
 	@Test
 	public void shouldBeAbleToReplaceExistingKey() {
-		final String key = "twelfth.key";
+		final String key = genRandKey();
 		keyspace.add(key, "blah");
 		final String value = "something else";
 		Assert.assertTrue(keyspace.replace(key, value));
@@ -136,9 +223,24 @@ public abstract class BaseKeyspaceTest {
 	@DataProvider
 	public Object[][] shouldNotBeAbleToAddNullsData() {
 		return new Object[][] { //
-		{ "third.key", null }, //
+		{ genRandKey(), null }, //
 				{ null, "something else" }, //
 				{ null, null } //
+		};
+	}
+
+	@Test(dataProvider = "shouldNotBeAbleToCheckAndSetWithNullsData", expectedExceptions = NullPointerException.class)
+	public void shouldNotBeAbleToCheckAndSetWithNulls(final String key, final String value) {
+		keyspace.checkAndSet(key, value, 43L);
+		Assert.fail("Expected exception!");
+	}
+
+	@DataProvider
+	protected Object[][] shouldNotBeAbleToCheckAndSetWithNullsData() {
+		return new Object[][] { //
+		{ null, "something" }, //
+				{ genRandKey(), null }, //
+				{ null, null }, //
 		};
 	}
 
@@ -155,10 +257,10 @@ public abstract class BaseKeyspaceTest {
 
 	@Test
 	public void shouldNotBeAbleToReplaceNewKey() {
-		Assert.assertFalse(keyspace.replace("thirteenth.key", "a value"));
+		Assert.assertFalse(keyspace.replace(genRandKey(), "a value"));
 	}
 
-	@Test(dataProvider = "shouldNotBeAbleToReplaceWithNullsData")
+	@Test(dataProvider = "shouldNotBeAbleToReplaceWithNullsData", expectedExceptions = NullPointerException.class)
 	public void shouldNotBeAbleToReplaceWithNulls(final String key, final String value) {
 		keyspace.replace(key, value);
 		Assert.fail("Expected exception!");
@@ -168,7 +270,22 @@ public abstract class BaseKeyspaceTest {
 	protected Object[][] shouldNotBeAbleToReplaceWithNullsData() {
 		return new Object[][] { //
 		{ null, "something" }, //
-				{ "eleventh.key", null }, //
+				{ genRandKey(), null }, //
+				{ null, null }, //
+		};
+	}
+	
+	@Test(dataProvider = "shouldNotBeAbleToSetWithNullsData", expectedExceptions = NullPointerException.class)
+	public void shouldNotBeAbleToSetWithNulls(final String key, final String value) {
+		keyspace.set(key, value);
+		Assert.fail("Expected exception!");
+	}
+
+	@DataProvider
+	protected Object[][] shouldNotBeAbleToSetWithNullsData() {
+		return new Object[][] { //
+		{ null, "something" }, //
+				{ genRandKey(), null }, //
 				{ null, null }, //
 		};
 	}
